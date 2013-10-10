86ab24e6f452a527bf7f53e36cc5d260dd106d94
Megan Davis <megan.davis@oberlin.edu>
2013-10-09 20:31:00 -0400

    created box model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010001947) do
+ActiveRecord::Schema.define(:version => 20131010003033) do
+
+  create_table "boxes", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "notebooks", :force => true do |t|
     t.string   "name"
