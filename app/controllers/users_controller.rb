f4ae4670d42714ccdba78bc426578e280eaa83da
peter-fogg <peter.fogg@oberlin.edu>
2013-10-12 17:09:51 -0400

    User signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    puts params
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
