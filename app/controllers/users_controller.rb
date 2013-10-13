f00f95759c25de93b1629fac9f176c80700766cb
peter-fogg <peter.fogg@oberlin.edu>
2013-10-13 18:46:50 -0400

    Minor fix.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      sign_in user
+      sign_in @user
       redirect_to @user
     else
       render 'new'
