9f0956d12a3a533362154b36cf66c25513b9034f
peter-fogg <peter.fogg@oberlin.edu>
2013-10-19 18:24:41 -0400

    Stop creating a new User token on save.
    Seriously, why the fuck did this ever make sense? I'm pretty sure the
    Rails tutorial tells you to, but it's completely broken to make a new
    remember token every time the model is saved (unless you also update
    the session cookie, WHICH YOU DON'T).

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = user.email.downcase }
-  before_save :create_remember_token
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -20,11 +19,5 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
-  end
-
-  private
-
-  def create_remember_token
-    self.remember_token = User.encrypt(User.new_remember_token)
   end
 end
