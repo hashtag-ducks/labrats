193eb922051d91e46445aa692a713c50f147d85d
peter-fogg <peter.fogg@oberlin.edu>
2013-10-19 18:52:46 -0400

    Creating Notebooks.

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,4 +1,18 @@
 class NotebooksController < ApplicationController
+  def new
+    @notebook = Notebook.new
+    @notebook.user = current_user
+  end
+
+  def create
+    @notebook = Notebook.new(params[:notebook])
+    if @notebook.save
+      redirect_to @notebook
+    else
+      render 'new'
+    end
+  end
+
   def index
     @notebooks = current_user.notebook.all
   end
