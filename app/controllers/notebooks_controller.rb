1e11419502ee7c7f5449fd0d175ae8719d7d8b82
peter-fogg <peter.fogg@oberlin.edu>
2013-10-20 11:37:51 -0400

    Deleting notebooks.

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,11 +1,13 @@
 class NotebooksController < ApplicationController
+  before_filter :correct_user, only: [:show, :delete]
+
   def new
     @notebook = Notebook.new
-    @notebook.user = current_user
   end
 
   def create
     @notebook = Notebook.new(params[:notebook])
+    @notebook.user = current_user
     if @notebook.save
       redirect_to @notebook
     else
@@ -19,5 +21,17 @@ class NotebooksController < ApplicationController
 
   def show
     @notebook = Notebook.find(params[:id])
+  end
+
+  def destroy
+    Notebook.find(params[:id]).delete
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def correct_user
+    @notebook = Notebook.find_by_id(params[:id])
+    redirect_to root_url unless current_user?(@notebook.user)
   end
 end
