2eaff056f833adf91d23e272e0519e2dfe562f1c
peter-fogg <peter.fogg@oberlin.edu>
2013-10-22 12:50:00 -0400

    Basic tests for static pages.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+
+  subject { page }
+
+  describe 'Home page' do
+    before { visit '/' }
+
+    it { should have_content('labrats') }
+    it { should have_selector('li a', text: "Sign In") }
+    it { should have_selector('li a', text: "Sign Up") }
+  end
+
+  describe 'Signin page' do
+    before { visit '/signin' }
+
+    it { should have_content('Sign In') }
+    it { should have_selector('input[type="password"]') }
+  end
+
+  describe 'Signup page' do
+    before { visit '/signup' }
+
+    it { should have_content('Create Account') }
+    it { should have_css('input[type="text"]', count: 3) }
+    it { should have_css('input[type="password"]', count: 2) }
+  end
+end
