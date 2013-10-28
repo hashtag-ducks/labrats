8a40cf7c0724da561e7f1b430338ac0c94723819
peter-fogg <peter.fogg@oberlin.edu>
2013-10-28 18:18:51 -0400

    Fix Heroku deploys.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Labrats
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Needed for Heroku to precompile assets properly.
+    config.initialize_on_precompile = false
   end
 end
