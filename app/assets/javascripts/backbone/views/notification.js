798c001aef74ce61a12a6f876d8537ff7743eaf4
peter-fogg <peter.fogg@oberlin.edu>
2013-12-09 14:35:11 -0500

    Only one notification at a time.

--- app/assets/javascripts/backbone/views/notification.js
+++ app/assets/javascripts/backbone/views/notification.js
@@ -12,19 +12,38 @@ Labrats.Views.Notification = Backbone.View.extend({
     },
 
     render: function() {
+        // We don't want multiple notifications in the same place, so
+        // we'll ensure that only one exists at a time.
+        var singleton = Labrats.Views.Notification.active;
+        if(singleton && singleton !== this) {
+            singleton.stopListening();
+            singleton.undelegateEvents();
+        }
         this.$el.html(_.template(this.tpl, this.options));
+        Labrats.Views.Notification.active = this;
     },
 
     show: function() {
         clearTimeout(this.hideTimeout);
         this.options.shown = true;
+        this.shownAt = new Date();
         this.hideTimeout = setTimeout(_.bind(this.hide, this), this.options.duration);
         this.render();
     },
 
     hide: function() {
-        clearTimeout(this.hideTimeout);
-        this.options.shown = false;
-        this.render();
+        // Make sure notification is shown for `this.duration`.
+        if(this.shownAt && this.options.duration <= new Date() - this.shownAt) {
+            this.options.shown = false;
+            delete this.shownAt;
+            this.render();
+        }
+        else {
+            clearTimeout(this.hideTimeout);
+            this.hideTimeout = setTimeout(
+                _.bind(this.hide, this),
+                this.options.duration - (new Date() - this.shownAt)
+            );
+        }
     }
 });
