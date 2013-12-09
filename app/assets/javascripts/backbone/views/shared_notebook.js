221c872a291a52e88a6933771edcf991623eb722
peter-fogg <peter.fogg@oberlin.edu>
2013-12-09 08:17:28 -0500

    Bring page-switching to shared notebooks.

--- app/assets/javascripts/backbone/views/shared_notebook.js
+++ app/assets/javascripts/backbone/views/shared_notebook.js
@@ -1,13 +1,43 @@
 Labrats.Views.SharedNotebook = Backbone.View.extend({
+    events: {
+        'click .page-select button': 'selectPage'
+    },
+
+    pages: [],
+
+    selectedPage: 0,
+
     initialize: function() {
         var self = this;
-        this.model.get('pages').forEach(function(page) {
-            var ele = $('<li></li>');
-            self.$el.children('ul.pages').append(ele);
+        this.model.get('pages').forEach(function(page, index) {
+            var ele = $('<div></div>');
+            self.$el.children('div.pages').append(ele);
             var view = new Labrats.Views.FilledPage({
                 model: page,
                 el: ele
             });
+            self.pages.push(view);
+            var li = '<li><button href="#" id="page-<%= index %>" class="btn btn-default">Page <%= index + 1 %></button></li>'
+            self.$el.find('ul.page-select').append(
+                $(_.template(li, {
+                    index: index
+                }))
+            );
         });
+        this.render();
+    },
+
+    render: function() {
+        this.$el.find('.page').hide();
+        if(this.pages.length > 0) {
+            this.$el.find('#page-' + this.pages[this.selectedPage].model.get('id')).show();
+            this.$el.find("#page-title").text('Page ' + (this.selectedPage + 1 ));
+        }
+    },
+
+    selectPage: function(event) {
+        event.preventDefault();
+        this.selectedPage = parseInt(event.currentTarget.getAttribute('id').split(/-/)[1]);
+        this.render();
     }
 });
