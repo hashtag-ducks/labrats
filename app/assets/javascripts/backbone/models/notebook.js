1b123c95899128626220d2752fd2dded92fe2704
peter-fogg <peter.fogg@oberlin.edu>
2013-12-12 17:12:17 -0500

    Use page orderings on the front end.

--- app/assets/javascripts/backbone/models/notebook.js
+++ app/assets/javascripts/backbone/models/notebook.js
@@ -10,14 +10,20 @@ Labrats.Models.Notebook = Backbone.Model.extend({
     parse: function(response) {
         // If `page_templates` is a key, this is being shown to the owner.
         if(response.page_templates) {
-          response.page_templates = new Labrats.Collections.PageTemplates(
-              _.map(response.page_templates, function(page_JSON) {
-                  return new Labrats.Models.PageTemplate(page_JSON, {parse: true});
-              })
-          );
+            response.page_templates.sort(function(p1, p2) {
+                return p1.ordering >= p2.ordering;
+            });
+            response.page_templates = new Labrats.Collections.PageTemplates(
+                _.map(response.page_templates, function(page_JSON) {
+                    return new Labrats.Models.PageTemplate(page_JSON, {parse: true});
+                })
+            );
         }
         // Otherwise, to a user with access.
         else {
+            response.pages.sort(function(p1, p2) {
+                return p1.ordering >= p2.ordering;
+            });
             response.pages = new Labrats.Collections.Pages(
                 _.map(response.pages, function(page_JSON) {
                     return new Labrats.Models.Page(page_JSON, {parse: true});
