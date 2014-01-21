515cd6402d9e664aad2109dadb85901e52225996
peter-fogg <peter.fogg@oberlin.edu>
2014-01-21 14:42:49 -0500

    Add notebook model tests.

--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -37,4 +37,52 @@ describe Notebook do
     @notebook.destroy
     expect(@notebook.page_templates.count).to equal(0)
   end
+
+  describe 'setup_defaults' do
+    it 'starts with no pages' do
+      expect(PageTemplate.where(notebook_id: @notebook.id).length).to equal(0)
+    end
+
+    it 'adds a page when called' do
+      @notebook.setup_defaults
+      expect(PageTemplate.where(notebook_id: @notebook.id).length).to equal(1)
+    end
+  end
+
+  describe 'add_user' do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'adds a user' do
+      users_count = @notebook.users.length
+      @notebook.add_user(@user)
+      expect(@notebook.users.length).to equal(users_count + 1)
+    end
+
+    it 'creates a copy of this notebook' do
+      @notebook.setup_defaults
+      @notebook.add_user(@user)
+      expect(@user.notebooks.length).to equal(1)
+      expect(@user.notebooks.first.owner_id).to equal(@notebook.owner_id)
+    end
+  end
+
+  describe 'remove_user' do
+    before do
+      @user = FactoryGirl.create(:user)
+      @notebook.add_user(@user)
+    end
+
+    it 'removes a user' do
+      users_count = @notebook.users.length
+      @notebook.remove_user(@user)
+      expect(@notebook.users.length).to equal(users_count - 1)
+    end
+
+    it "destroys that user's copy" do
+      @notebook.remove_user(@user)
+      expect(@user.notebooks.length).to equal(0)
+    end
+  end
 end
