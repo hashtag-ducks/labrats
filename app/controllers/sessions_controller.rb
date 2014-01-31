26ac548cd2b89de33b11a6193ce4c5c11f5abe01
Oren Shoham <oshoham@oberlin.edu>
2014-01-31 14:40:42 -0500

    Added SessionsController tests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by_email(params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
-            sign_in user
-            redirect_back_or user
+        @user = User.find_by_email(params[:session][:email].downcase)
+        if @user && @user.authenticate(params[:session][:password])
+            sign_in @user
+            redirect_back_or @user
         else
             flash.now[:error] = "Invalid email/password combo"
             render 'new'
